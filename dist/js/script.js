f8c9ac045b32a04f1f6cadfbab6cff92ea86254f
Swadesh Ranjan Rout <swadeshrout2@gmail.com>
2018-04-04 21:28:07 +0530

    Update script.js

--- dist/js/script.js
+++ dist/js/script.js
@@ -57,4 +57,55 @@ $(document).ready(function(){
     )
     
     
+    $("#link1").click(function(){
+        $("#html").show("slow");
+        $("#html1").show("slow");
+        $("#boot").show("slow");
+        $("#js").show("slow");
+        $("#jquery").show("slow");
+        $("#jquery1").show("slow");
+    })
+    
+    
+    $("#link2").click(function(){
+        $("#html").show("slow");
+        $("#html1").show("slow");
+        $("#boot").hide("slow");
+        $("#js").hide("slow");
+        $("#jquery").hide("slow");
+        $("#jquery1").hide("slow");
+        
+    })
+    
+    $("#link3").click(function(){
+        $("#html").hide("slow");
+        $("#html1").hide("slow");
+        $("#boot").show("slow");
+        $("#js").hide("slow");
+        $("#jquery").hide("slow");
+        $("#jquery1").hide("slow");
+        
+    })
+    
+    $("#link4").click(function(){
+        $("#html").hide("slow");
+        $("#html1").hide("slow");
+        $("#boot").hide("slow");
+        $("#js").show("slow");
+        $("#jquery").hide("slow");
+        $("#jquery1").hide("slow");
+        
+    })
+    
+    
+    $("#link5").click(function(){
+        $("#html").hide("slow");
+        $("#html1").hide("slow");
+        $("#boot").hide("slow");
+        $("#js").hide("slow");
+        $("#jquery").show("slow");
+        $("#jquery1").show("slow");
+        
+    })
+    
 });
